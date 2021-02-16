0973c4a027d280f51b16a3415c7624c8c70e7e79
Professional <eli.pond21@mail.ru>
2021-02-16 10:49:25 +0300

    Выполнено выпадающее меню и форма входа и регистрации через Popup с помощью библиотке jquery.

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $(`.up`).hide();
+  $(`.clickMenu`).click(function(){
+   
+    $(`.visibleMenu`).toggle(400);
+      
+     $(`.up`).toggle();
+     $(".down").toggle();
+  });  
+});
+
+// открыть по кнопке
+$('.js-button-campaign').click(function() {
+
+    $('.js-overlay-campaign').fadeIn();
+    $('.js-overlay-campaign').addClass('disabled');
+});
+
+// закрыть на крестик
+$('.js-close-campaign').click(function() {
+    $('.js-overlay-campaign').fadeOut();
+
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+    var popup = $('.js-popup-campaign');
+    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+        $('.js-overlay-campaign').fadeOut();
+
+    }
+});
